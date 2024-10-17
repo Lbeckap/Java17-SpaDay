a7c35b339f122bec9f40542eb0b915d6821153ba
Lori <148814155+Lbeckap@users.noreply.github.com>
2024-10-17 14:39:45 -0400

    studio

---
+++ src/main/java/org/launchcode/controllers/UserController.java
@@ -0,0 +1,36 @@
+package org.launchcode.controllers;
+
+import org.launchcode.models.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("user")
+public class UserController {
+
+    @GetMapping
+    public String renderIndex() {
+        return "user/index";
+    }
+
+    @GetMapping("add")
+    public String displayAddUserForm() {
+        return "user/add";
+    }
+
+    @PostMapping("add")
+    public String processAddUserForm(Model model,@ModelAttribute User user, String verify) {
+        if (user.getPassword().equals(verify)) {
+            return "user/index";
+        } else {
+            model.addAttribute("error", "Passwords must match. Please try again!");
+            model.addAttribute("username", user.getUsername());
+            model.addAttribute("email", user.getEmail());
+            return "user/add";
+        }
+    }
+}
---
+++ src/main/java/org/launchcode/models/User.java
@@ -0,0 +1,57 @@
+package org.launchcode.models;
+
+import java.util.Objects;
+
+public class User {
+    private int id;
+    private static int nextId = 1;
+
+    private String username;
+    private String email;
+    private String password;
+
+    public User(String username, String email, String password) {
+        this.id = nextId;
+        nextId++;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
